78bfa2f3f2bb6c03b03b8005951ce518dc4226d6
Amit Sarkar <amitskr6@gmail.com>
2024-01-14 02:45:52 +0530

    Added Auth Screen, Initialized Router and custom text fields

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:amazon_clone/constants/global_variables.dart';
+import 'package:amazon_clone/features/auth/screens/auth_screen.dart';
+import 'package:amazon_clone/router.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,34 +14,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
           scaffoldBackgroundColor: GlobalVariables.backgroundColor,
           colorScheme: const ColorScheme.light(
-            primary: GlobalVariables.secondaryColor, 
+            primary: GlobalVariables.secondaryColor,
           ),
           appBarTheme: const AppBarTheme(
               elevation: 0,
               iconTheme: IconThemeData(
                 color: Colors.black,
               ))),
-      home: Scaffold(
-          appBar: AppBar(
-            title: const Text("Hello"),
-          ),
-          body: Column(
-            children: [
-              const Center(
-                child: Text(
-                  'Flutter Demo Home Page',
-                ),
-              ),
-              ElevatedButton(
-                onPressed: () {},
-                child: const Text("Click"),
-              ),
-            ],
-          )),
+      onGenerateRoute: (settings) => generateRoute(settings),
+      home: AuthScreen(),
     );
   }
 }
